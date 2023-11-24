31d0734701a8d3baac75afd036856c4be612123f
TemurEShtemirov <temureshtemirov10@gmail.com>
2023-11-24 15:16:02 +0500

    start home page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,13 +5,13 @@ export default function Header() {
   return (
     <header>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand ms-3" href="#">
           <img src={Logo} alt="Logo" />
         </a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="collapse navbar-collapse justify-content-center align-items-center" id="navbarNav">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
               <a className="nav-link" href="#">Home</a>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Banner from '../../assets/bannner.png'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className='container mt-5'>
+      <main>
+        <section className='section1'>
+          <img src={Banner} alt={Banner} />
+        </section>
+      </main>
+    </div>
   )
 }
